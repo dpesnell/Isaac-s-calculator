6c79f5010b927f6809eb293d33f9ec32717b37a0
Darin Pesnell <63257529+dpesnell@users.noreply.github.com>
2020-12-23 07:02:04 -0500

    Add files via upload

---
+++ isaac.py
@@ -0,0 +1,11 @@
+#!/bin/env python
+
+print("Enter the first number")
+number_1 = input()
+
+print("Enter the second number")
+number_2 = input()
+
+
+number_3 = int(number_1) + int(number_2)
+print(number_1 + " plus " + number_2 + " equals " + str(number_3))
